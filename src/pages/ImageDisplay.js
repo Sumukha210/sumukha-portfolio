db2dcd025c98269fcb64e20866d80bbf5152f9d9
Sumukha210 <sumukhakb@gmail.com>
2021-02-06 01:16:39 +0530

    adding external pdf link

--- src/pages/ImageDisplay.js
+++ src/pages/ImageDisplay.js
@@ -4,7 +4,9 @@ import { graphql, useStaticQuery, Link } from "gatsby"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
+import { BiLinkExternal } from "react-icons/bi"
 
+//BiLinkExternal
 const ImageDisplay = () => {
   const {
     file: {
@@ -14,9 +16,20 @@ const ImageDisplay = () => {
 
   return (
     <div className="imageDisplay">
-      <Button as={Link} className="customBtn mb-3" to="/">
-        Back
-      </Button>
+      <div className="d-flex align-items-center justify-content-between">
+        <Button as={Link} className="customBtn mb-3" to="/">
+          Back
+        </Button>
+        <Button
+          as="a"
+          href="https://www.docdroid.net/k4iBD2N/sumukha-resume-pdf"
+          target="_blank"
+          variant="info"
+          className="customBtn PdfBtn"
+        >
+          <BiLinkExternal className="mr-2" /> PDF
+        </Button>
+      </div>
       <Row className="justify-content-center flex-column mt-5">
         <h1 className="main-title ml-3">Resume</h1>
         <Col md={10}>
